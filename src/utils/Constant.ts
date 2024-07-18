c88e6c6522236a13316bbc704bf486e97ce4b677
DevKike <alvaronarvaez151@gmail.com>
2024-07-18 17:51:58 -0500

    Feat: add typeorm config and dotenv for constants

---
+++ src/utils/Constant.ts
@@ -0,0 +1,15 @@
+import { config } from "dotenv";
+config();
+
+export class Constant {
+  // Server
+  static readonly PORT = process?.env?.PORT || 3001;
+
+  // Database
+  static readonly DB_TYPE = process?.env?.DB_TYPE || "mysql";
+  static readonly DB_HOST = process?.env?.DB_HOST;
+  static readonly DB_PORT = process?.env?.DB_PORT;
+  static readonly DB_USERNAME = process?.env?.DB_USERNAME;
+  static readonly DB_PASSWORD = process?.env?.DB_PASSWORD;
+  static readonly DB_DATABASE_NAME = process?.env?.DB_DATABASE_NAME;
+}
