ccecdf3c7dc5ff84a1605ea023542c27cbc7bf14
DevKike <alvaronarvaez151@gmail.com>
2024-07-19 18:34:00 -0500

    Refactor: change variable to db_name

--- src/utils/Constant.ts
+++ src/utils/Constant.ts
@@ -11,5 +11,5 @@ export class Constant {
   static readonly DB_PORT = process?.env?.DB_PORT;
   static readonly DB_USERNAME = process?.env?.DB_USERNAME;
   static readonly DB_PASSWORD = process?.env?.DB_PASSWORD;
-  static readonly DB_DATABASE_NAME = process?.env?.DB_DATABASE_NAME;
+  static readonly DB_NAME = process?.env?.DB_NAME;
 }
