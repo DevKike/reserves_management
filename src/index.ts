1f0816f41670eeda6b4ebf5c8a7773429d2f748a
DevKike <alvaronarvaez151@gmail.com>
2024-07-20 18:05:22 -0500

    Feat: add init for user repository and user service implementation

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,17 @@
 import { app } from "./app/app";
 import { Constant } from "./utils/Constant";
-import { AppDataSource } from "./config/typeorm";
+import { appDataSource } from "./config/typeorm";
+import { ImplUserRepository } from "./modules/user/infrastructure/repository/ImplUserRepository";
+import { ImplUserService } from "./modules/user/infrastructure/service/ImplUserService";
 
 app.listen(Constant.PORT, async () => {
   try {
-    await AppDataSource.initialize();
     console.log(`Server running at port: ${Constant.PORT}`);
+    await appDataSource.initialize();
     console.log("Data source has been initialized!");
+
+    const userRepository = new ImplUserRepository(appDataSource);
+    const userService = new ImplUserService(userRepository);
   } catch (error) {
     throw error;
   }
