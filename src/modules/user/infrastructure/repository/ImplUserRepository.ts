bd87e13b307cf69c9c209123cdf1dd00e0576c79
DevKike <alvaronarvaez151@gmail.com>
2024-07-20 18:02:47 -0500

    Feat: add an implementation for user repository

---
+++ src/modules/user/infrastructure/repository/ImplUserRepository.ts
@@ -0,0 +1,17 @@
+import { DataSource, Repository } from "typeorm";
+import { IUser } from "../../domain/entity/IUser";
+import { User } from "../entity/User";
+import { IUserRepository } from "../../domain/repository/IUserRepository";
+import { appDataSource } from "../../../../config/typeorm";
+
+export class ImplUserRepository implements IUserRepository {
+  private _userRepository: Repository<User>;
+
+  constructor(dataSource: DataSource = appDataSource) {
+    this._userRepository = dataSource.getRepository(User);
+  }
+
+  async create(user: IUser): Promise<void> {
+    await this._userRepository.save(user);
+  }
+}
