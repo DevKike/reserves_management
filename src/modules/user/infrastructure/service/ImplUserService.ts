e963e6e2802bacc3167877bfe1935914e308669b
DevKike <alvaronarvaez151@gmail.com>
2024-07-20 18:02:30 -0500

    Feat: add an implementation for user service

---
+++ src/modules/user/infrastructure/service/ImplUserService.ts
@@ -0,0 +1,20 @@
+import { IUser } from "../../domain/entity/IUser";
+import { IUserRepository } from "../../domain/repository/IUserRepository";
+import { IUserService } from "../../domain/service/IUserService";
+import { ImplUserRepository } from "../repository/ImplUserRepository";
+
+export class ImplUserService implements IUserService {
+  private _userRepository: IUserRepository;
+  
+  constructor(userRepository: IUserRepository) {
+    this._userRepository = userRepository;
+  }
+
+  async createUser(user: IUser): Promise<void> {
+    try {
+      await this._userRepository.create(user);
+    } catch (error) {
+      throw error;
+    }
+  }
+}
