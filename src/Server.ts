2a799eea1dcddfcfa04748154160a835b77e06bf
DevKike <alvaronarvaez151@gmail.com>
2024-07-20 20:28:40 -0500

    Feat: add routes for endpoints

--- src/Server.ts
+++ src/Server.ts
@@ -1,19 +1,25 @@
 import { App } from "./app/App";
 import { appDataSource } from "./config/typeorm";
+import { ImplUserUseCase } from "./modules/user/application/ImplUserUseCase";
 import { IUserRepository } from "./modules/user/domain/repository/IUserRepository";
 import { IUserService } from "./modules/user/domain/service/IUserService";
 import { ImplUserRepository } from "./modules/user/infrastructure/repository/ImplUserRepository";
 import { ImplUserService } from "./modules/user/infrastructure/service/ImplUserService";
+import { RouterManager } from "./modules/user/RouterManager";
 
 class Server {
   private _app: App;
   private _userRepository: IUserRepository;
   private _userService: IUserService;
+  private _userUseCase: ImplUserUseCase;
+  private _routerManager: RouterManager;
 
   constructor() {
     this._app = new App();
     this._userRepository = new ImplUserRepository(appDataSource);
     this._userService = new ImplUserService(this._userRepository);
+    this._userUseCase = new ImplUserUseCase(this._userService);
+    this._routerManager = new RouterManager(this._app.getApp(), this._userUseCase);
   }
 
   private async initDataSource(): Promise<void> {
@@ -27,6 +33,7 @@ class Server {
 
   private startServer(): void {
     const application = this._app.getApp();
+    this._routerManager.manageRoutes();
     
     application.listen(application.get("PORT"), () => {
       try {
