42f0ebf653218843ef278f6655a3ac8e23769913
DevKike <alvaronarvaez151@gmail.com>
2024-07-20 19:03:00 -0500

    Refactor: change minor things

--- src/Server.ts
+++ src/Server.ts
@@ -1,4 +1,4 @@
-import { App } from "./app/app";
+import { App } from "./app/App";
 import { appDataSource } from "./config/typeorm";
 import { IUserRepository } from "./modules/user/domain/repository/IUserRepository";
 import { IUserService } from "./modules/user/domain/service/IUserService";
@@ -10,14 +10,10 @@ class Server {
   private _userRepository: IUserRepository;
   private _userService: IUserService;
 
-  constructor(
-    _app: App,
-    _userRepository: IUserRepository,
-    _userService: IUserService
-  ) {
-    this._app = _app;
-    this._userRepository = _userRepository;
-    this._userService = _userService;
+  constructor() {
+    this._app = new App();
+    this._userRepository = new ImplUserRepository(appDataSource);
+    this._userService = new ImplUserService(this._userRepository);
   }
 
   private async initDataSource(): Promise<void> {
@@ -46,9 +42,5 @@ class Server {
   }
 }
 
-const app = new App();
-const userRepository = new ImplUserRepository(appDataSource);
-const userService = new ImplUserService(userRepository);
-
-const server = new Server(app, userRepository, userService);
+const server = new Server();
 server.init();
